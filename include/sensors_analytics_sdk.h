e30fa07ebc88d9546fa02c665ab6011f7fac5c4f
彭远洋 <pengyuanyang@sensorsdata.cn>
2020-06-29 20:09:53 +0800

    Release 0.6

--- include/sensors_analytics_sdk.h
+++ include/sensors_analytics_sdk.h
@@ -27,7 +27,7 @@
 #include <utility>
 #include <vector>
 
-#define SA_SDK_VERSION "0.4"
+#define SA_SDK_VERSION "0.6"
 #define SA_SDK_NAME "SensorsAnalytics CPP SDK"
 #define SA_SDK_FULL_NAME SA_SDK_NAME " " SA_SDK_VERSION
 
@@ -46,256 +46,277 @@ typedef __int64 int64_t;
 
 namespace sensors_analytics {
 
-    using std::string;
+using std::string;
 
-    class Sdk;
+class Sdk;
 
-    class DefaultConsumer;
+class DefaultConsumer;
 
-    class PropertiesNode;
-    namespace utils {
-        class ObjectNode;
-    }
+class PropertiesNode;
+namespace utils {
+class ObjectNode;
+}
 
-    class Sdk {
-    public:
-        // 初始化 SDK
-        // data_file_path: 暂存文件路径，用于将未发送的数据临时保存在磁盘
-        // server_url: 神策服务器地址
-        // distinct_id: 标识一个用户的 ID
-        // is_login_id: distinct_id 参数传值是否是一个“登录 ID”
-        // max_staging_record_count: 在发送队列中最多保存的数据条数，若当前未发送数据条数达到该值，
-        //                           新埋点记录将淘汰最早的一个记录
-        static bool Init(const string &data_file_path, const string &server_url,
-                         const string &distinct_id, bool is_login_id, int max_staging_record_count);
+class Sdk {
+ public:
+  // 初始化 SDK
+  // data_file_path: 暂存文件路径，用于将未发送的数据临时保存在磁盘
+  // server_url: 神策服务器地址
+  // distinct_id: 标识一个用户的 ID
+  // is_login_id: distinct_id 参数传值是否是一个“登录 ID”
+  // max_staging_record_count: 在发送队列中最多保存的数据条数，若当前未发送数据条数达到该值，
+  //                           新埋点记录将淘汰最早的一个记录
+  static bool Init(const string &data_file_path,
+                   const string &server_url,
+                   const string &distinct_id,
+                   bool is_login_id,
+                   int max_staging_record_count);
 
-        static void EnableLog(bool enable);
+  static void EnableLog(bool enable);
 
-        // 将所有本地数据发送到服务端
-        static bool Flush();
+  // 将所有本地数据发送到服务端
+  static bool Flush();
 
-        // 触发一次发送，发送最多 part_size 条数据
-        // 当 drop_failed_record 为 true 时，发送失败则丢弃这些数据不再发送
-        // 当 drop_failed_record 为 false 时，发送失败仍保留在队列里，下次再试
-        static bool FlushPart(size_t part_size, bool drop_failed_record);
+  // 触发一次发送，发送最多 part_size 条数据
+  // 当 drop_failed_record 为 true 时，发送失败则丢弃这些数据不再发送
+  // 当 drop_failed_record 为 false 时，发送失败仍保留在队列里，下次再试
+  static bool FlushPart(size_t part_size, bool drop_failed_record);
 
-        // 清空本地发送队列，包括内存和文件
-        static void ClearQueue();
+  // 清空本地发送队列，包括内存和文件
+  static void ClearQueue();
 
-        // 析构 SDK 实例，回到 Init 前的状态，未发送的数据将暂存到磁盘
-        static void Shutdown();
+  // 析构 SDK 实例，回到 Init 前的状态，未发送的数据将暂存到磁盘
+  static void Shutdown();
 
-        // 设置每个事件都带有的一些公共属性
-        static void RegisterSuperProperties(const PropertiesNode &properties);
+  // 设置每个事件都带有的一些公共属性
+  static void RegisterSuperProperties(const PropertiesNode &properties);
 
-        // 清除公共属性
-        static void ClearSuperProperties();
+  // 清除公共属性
+  static void ClearSuperProperties();
 
-        // 记录行为事件
-        static void Track(const string &event_name);
+  // 记录行为事件
+  static void Track(const string &event_name);
 
-        static void Track(const string &event_name, const PropertiesNode &properties);
+  static void Track(const string &event_name, const PropertiesNode &properties);
 
-        //记录物品元数据事件
-        static void ItemSet(const string &item_type, const string &item_id, PropertiesNode &properties);
+  //记录物品元数据事件
+  static void ItemSet(const string &item_type,
+                      const string &item_id,
+                      PropertiesNode &properties);
 
-        static void ItemDelete(const string &item_type, const string &item_id);
+  static void ItemDelete(const string &item_type, const string &item_id);
 
 
-        // 记录安装事件，安装后首次启动调用，服务端可以根据 IP 等匹配渠道信息
-        static void TrackInstallation(const string &event_name, const PropertiesNode &properties);
+  // 记录安装事件，安装后首次启动调用，服务端可以根据 IP 等匹配渠道信息
+  static void
+  TrackInstallation(const string &event_name, const PropertiesNode &properties);
 
-        // 记录“登录”，将登陆前后 ID 绑定。之后会使用 login_id 作为 distinct_id
-        static void Login(const string &login_id);
+  // 记录“登录”，将登陆前后 ID 绑定。之后会使用 login_id 作为 distinct_id
+  static void Login(const string &login_id);
 
-        // 设置使用的 distinct_id
-        static void Identify(const string &distinct_id, bool is_login_id);
+  // 设置使用的 distinct_id
+  static void Identify(const string &distinct_id, bool is_login_id);
 
-        // 设置一个用户属性，已存在则覆盖
-        static void ProfileSet(const PropertiesNode &properties);
+  // 设置一个用户属性，已存在则覆盖
+  static void ProfileSet(const PropertiesNode &properties);
 
-        static void ProfileSetString(const string &property_name, const string &str_value);
+  static void
+  ProfileSetString(const string &property_name, const string &str_value);
 
-        static void ProfileSetNumber(const string &property_name, int number_value);
+  static void ProfileSetNumber(const string &property_name, int number_value);
 
-        static void ProfileSetNumber(const string &property_name, double number_value);
+  static void
+  ProfileSetNumber(const string &property_name, double number_value);
 
-        static void ProfileSetBool(const string &property_name, bool bool_value);
+  static void ProfileSetBool(const string &property_name, bool bool_value);
 
-        // 设置一个用户属性，仅当之前此属性没有值时该操作有效
-        static void ProfileSetOnce(const PropertiesNode &properties);
+  // 设置一个用户属性，仅当之前此属性没有值时该操作有效
+  static void ProfileSetOnce(const PropertiesNode &properties);
 
-        static void ProfileSetOnceString(const string &property_name, const string &str_value);
+  static void
+  ProfileSetOnceString(const string &property_name, const string &str_value);
 
-        static void ProfileSetOnceNumber(const string &property_name, int number_value);
+  static void
+  ProfileSetOnceNumber(const string &property_name, int number_value);
 
-        static void ProfileSetOnceNumber(const string &property_name, double number_value);
+  static void
+  ProfileSetOnceNumber(const string &property_name, double number_value);
 
-        static void ProfileSetOnceBool(const string &property_name, bool bool_value);
+  static void ProfileSetOnceBool(const string &property_name, bool bool_value);
 
-        // 为用户的一个或多个数值类型的属性累加一个数值
-        static void ProfileIncrement(const PropertiesNode &properties);
+  // 为用户的一个或多个数值类型的属性累加一个数值
+  static void ProfileIncrement(const PropertiesNode &properties);
 
-        static void ProfileIncrement(const string &property_name, int number_value);
+  static void ProfileIncrement(const string &property_name, int number_value);
 
-        // 为用户的一个或多个数组类型的属性追加字符串
-        static void ProfileAppend(const PropertiesNode &properties);
+  // 为用户的一个或多个数组类型的属性追加字符串
+  static void ProfileAppend(const PropertiesNode &properties);
 
-        static void ProfileAppend(const string &property_name, const string &str_value);
+  static void
+  ProfileAppend(const string &property_name, const string &str_value);
 
-        ~Sdk();
+  // 使用追加的方式把队列中数据添加到本地文件中，默认是 false
+  static void AppendRecordsToDisk(bool enable);
 
-    private:
-        Sdk(const string &server_url,
-            const string &data_file_path,
-            int max_staging_record_count,
-            const string &distinct_id,
-            bool is_login_id);
+  // 将队列中的所有数据直接追加到本地文件中，并清空队列内容
+  // 只有当开启 AppendRecordsToDisk 时调用此接口才有效
+  static void DumpAllRecordsToDisk();
 
-        Sdk(const Sdk &);
+  ~Sdk();
 
-        Sdk &operator=(const Sdk &);
+ private:
+  Sdk(const string &server_url,
+      const string &data_file_path,
+      int max_staging_record_count,
+      const string &distinct_id,
+      bool is_login_id);
 
-        bool AddEvent(const string &action_type,
-                      const string &event_name,
-                      const utils::ObjectNode &properties,
-                      const string &distinct_id,
-                      const string &original_id);
+  Sdk(const Sdk &);
 
-        bool AddItemEvent(const string &action_type,
-                      const string &item_type,
-                          const string &item_id,
-                      const utils::ObjectNode &properties);
+  Sdk &operator=(const Sdk &);
 
-        void ResetSuperProperties();
+  bool AddEvent(const string &action_type,
+                const string &event_name,
+                const utils::ObjectNode &properties,
+                const string &distinct_id,
+                const string &original_id);
 
-        static bool AssertProperties(const utils::ObjectNode &properties);
+  bool AddItemEvent(const string &action_type,
+                    const string &item_type,
+                    const string &item_id,
+                    const utils::ObjectNode &properties);
 
-        static bool AssertKey(const string &type, const string &key);
+  void ResetSuperProperties();
 
-        static bool AssertId(const string &type, const string &key);
+  static bool AssertProperties(const utils::ObjectNode &properties);
 
-        static Sdk *instance_;
-        PropertiesNode *super_properties_;
-        string distinct_id_;
-        bool is_login_id_;
-        DefaultConsumer *consumer_;
-    };
+  static bool AssertKey(const string &type, const string &key);
 
-    namespace utils {
+  static bool AssertId(const string &type, const string &key);
 
-        class ObjectNode {
-        public:
-            void SetNumber(const string &property_name, int32_t value);
+  static Sdk *instance_;
+  PropertiesNode *super_properties_;
+  string distinct_id_;
+  bool is_login_id_;
+  DefaultConsumer *consumer_;
+};
 
-            void SetNumber(const string &property_name, int64_t value);
+namespace utils {
 
-            void SetNumber(const string &property_name, double value);
+class ObjectNode {
+ public:
+  void SetNumber(const string &property_name, int32_t value);
 
-            void SetString(const string &property_name, const string &value);
+  void SetNumber(const string &property_name, int64_t value);
 
-            void SetString(const string &property_name, const char *value);
+  void SetNumber(const string &property_name, double value);
 
-            void SetBool(const string &property_name, bool value);
+  void SetString(const string &property_name, const string &value);
 
-            void SetList(const string &property_name, const std::vector<string> &value);
+  void SetString(const string &property_name, const char *value);
 
-            void SetDateTime(const string &property_name, time_t seconds, int milliseconds);
+  void SetBool(const string &property_name, bool value);
 
-            // 字符串格式需要是: 2018-09-07 16:30:22.567
-            void SetDateTime(const string &property_name, const string &value);
+  void SetList(const string &property_name, const std::vector<string> &value);
 
-            void Clear();
+  void
+  SetDateTime(const string &property_name, time_t seconds, int milliseconds);
 
-            virtual void SetObject(const string &property_name, const ObjectNode &value);
+  // 字符串格式需要是: 2018-09-07 16:30:22.567
+  void SetDateTime(const string &property_name, const string &value);
 
-            static string ToJson(const ObjectNode &node);
+  void Clear();
 
-        protected:
-            ObjectNode();
+  virtual void SetObject(const string &property_name, const ObjectNode &value);
 
-            class ValueNode;
+  static string ToJson(const ObjectNode &node);
 
-            std::map<string, ValueNode> properties_map_;
+ protected:
+  ObjectNode();
 
-        private:
-            static void DumpNode(const ObjectNode &node, string *buffer);
+  class ValueNode;
 
-            void MergeFrom(const ObjectNode &another_node);
+  std::map<string, ValueNode> properties_map_;
 
-            friend class ::sensors_analytics::Sdk;
+ private:
+  static void DumpNode(const ObjectNode &node, string *buffer);
 
-            enum ValueNodeType {
-                NUMBER,
-                INT,
-                STRING,
-                LIST,
-                DATETIME,
-                BOOL,
-                OBJECT,
-                UNKNOWN,
-            };
-        };
+  void MergeFrom(const ObjectNode &another_node);
 
-        class ObjectNode::ValueNode {
-        public:
-            ValueNode() : node_type_(UNKNOWN) {}
+  friend class ::sensors_analytics::Sdk;
 
-            explicit ValueNode(double value);
+  enum ValueNodeType {
+    NUMBER,
+    INT,
+    STRING,
+    LIST,
+    DATETIME,
+    BOOL,
+    OBJECT,
+    UNKNOWN,
+  };
+};
 
-            explicit ValueNode(int64_t value);
+class ObjectNode::ValueNode {
+ public:
+  ValueNode() : node_type_(UNKNOWN) {}
 
-            explicit ValueNode(const string &value);
+  explicit ValueNode(double value);
 
-            explicit ValueNode(bool value);
+  explicit ValueNode(int64_t value);
 
-            explicit ValueNode(const ObjectNode &value);
+  explicit ValueNode(const string &value);
 
-            explicit ValueNode(const std::vector<string> &value);
+  explicit ValueNode(bool value);
 
-            ValueNode(time_t seconds, int milliseconds);
+  explicit ValueNode(const ObjectNode &value);
 
-            static void ToStr(const ValueNode &node, string *buffer);
+  explicit ValueNode(const std::vector<string> &value);
 
-        private:
-            static void DumpString(const string &value, string *buffer);
+  ValueNode(time_t seconds, int milliseconds);
 
-            static void DumpList(const std::vector<string> &value, string *buffer);
+  static void ToStr(const ValueNode &node, string *buffer);
 
-            static void DumpDateTime(const time_t &seconds, int milliseconds, string *buffer);
+ private:
+  static void DumpString(const string &value, string *buffer);
 
-            static void DumpNumber(double value, string *buffer);
+  static void DumpList(const std::vector<string> &value, string *buffer);
 
-            static void DumpNumber(int64_t value, string *buffer);
+  static void
+  DumpDateTime(const time_t &seconds, int milliseconds, string *buffer);
 
-            friend class ::sensors_analytics::Sdk;
+  static void DumpNumber(double value, string *buffer);
 
-            ValueNodeType node_type_;
+  static void DumpNumber(int64_t value, string *buffer);
 
-            union UnionValue {
-                double number_value;
-                bool bool_value;
-                struct {
-                    std::time_t seconds;
-                    int milliseconds;
-                } date_time_value;
-                int64_t int_value;
+  friend class ::sensors_analytics::Sdk;
 
-                UnionValue() { memset(this, 0, sizeof(UnionValue)); }
-            } value_;
+  ValueNodeType node_type_;
 
-            string string_data_;
-            std::vector<string> list_data_;
-            ObjectNode object_data_;
-        };
-    }  // namespace utils
+  union UnionValue {
+    double number_value;
+    bool bool_value;
+    struct {
+      std::time_t seconds;
+      int milliseconds;
+    } date_time_value;
+    int64_t int_value;
 
-    class PropertiesNode : public utils::ObjectNode {
-    private:
-        friend class Sdk;
+    UnionValue() { memset(this, 0, sizeof(UnionValue)); }
+  } value_;
 
-        void SetObject(const string &property_name, const ObjectNode &value);
-    };
+  string string_data_;
+  std::vector<string> list_data_;
+  ObjectNode object_data_;
+};
+}  // namespace utils
+
+class PropertiesNode: public utils::ObjectNode {
+ private:
+  friend class Sdk;
+
+  void SetObject(const string &property_name, const ObjectNode &value);
+};
 
 }  // namespace sensors_analytics
 
